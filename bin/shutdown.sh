8ce12c32d607f954f2affc50e1c3993b3983865c
Ivan Kreso <kreso.ivan@gmail.com>
2013-04-28 17:04:14 +0200

    removec TagHighlight

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gxmessage "You sure madafaka?" -center -title "Take action" -font "Sans bold 10" -default "Cancel" -buttons "_Cancel":1,"_Shutdown":2 >/dev/null
+gxmessage "Confirm shutdown..." -center -title "Take action" -font "Sans bold 10" -default "Cancel" -buttons "_Cancel":1,"_Shutdown":2 >/dev/null
 
 case $? in
 	1)
